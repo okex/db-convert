6741ec4dddc0d6388b08a6c88a0cae79785423c8
ylsGit <bzwdyls@163.com>
2022-04-28 16:59:28 +0800

    update for boltdb

--- main.go
+++ main.go
@@ -8,54 +8,68 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/tecbot/gorocksdb"
 	tmdb "github.com/tendermint/tm-db"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-	fmt.Println(os.Args[1:])
+	fmt.Println(os.Args)
 
-	// /data/
-	fs, err := ioutil.ReadDir(os.Args[1])
+	sdbType := os.Args[1]
+	ddbType := os.Args[2]
+	sdbDir := os.Args[3]
+	ddbDir := os.Args[4]
+
+	// */data/*
+	fs, err := ioutil.ReadDir(sdbDir)
 	if err != nil {
 		panic(err)
 	}
 
 	for _, f := range fs {
-		if f.IsDir() {
+		if f.IsDir() && strings.HasSuffix(f.Name(), ".db") {
 			name := strings.Split(f.Name(), ".")[0]
 			wg.Add(1)
 			go func(name, leveldbDir, rocksdbDir string) {
 				defer wg.Done()
 
 				log.Printf("convert %s start...\n", name)
-				ldb, err := tmdb.NewGoLevelDB(name, leveldbDir)
-				if err != nil {
-					panic(err)
-				}
-				rdb, err := tmdb.NewRocksDB(name, rocksdbDir)
+				sdb, err := tmdb.NewDB(name, tmdb.BackendType(sdbType), sdbDir)
 				if err != nil {
 					panic(err)
 				}
 
-				iter, err := ldb.Iterator(nil, nil)
+				if _, err := os.Stat(ddbDir); os.IsNotExist(err) {
+					if err := os.MkdirAll(ddbDir, 0755); err != nil {
+						panic(err)
+					}
+				}
+				ddb, err := tmdb.NewDB(name, tmdb.BackendType(ddbType), ddbDir)
 				if err != nil {
 					panic(err)
 				}
 
-				for ; iter.Valid(); iter.Next() {
-					rdb.Set(iter.Key(), iter.Value())
-				}
-				iter.Close()
+				convert(sdb, ddb)
 				log.Printf("convert %s end.\n", name)
-				log.Printf("compact %s start...\n", name)
-				rdb.DB().CompactRange(gorocksdb.Range{})
-				log.Printf("compact %s end.\n", name)
-			}(name, os.Args[1], os.Args[2])
+				//log.Printf("compact %s start...\n", name)
+				//rdb.DB().CompactRange(gorocksdb.Range{})
+				//log.Printf("compact %s end.\n", name)
+			}(name, sdbDir, ddbDir)
 		}
 	}
 
 	wg.Wait()
+}
+
+func convert(sdb, ddb tmdb.DB) {
+	iter, err := sdb.Iterator(nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for ; iter.Valid(); iter.Next() {
+		ddb.Set(iter.Key(), iter.Value())
+	}
+	iter.Close()
 }
